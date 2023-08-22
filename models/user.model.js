e95912442a13b3852e7de4ae532375bfffc0ea08
Kevin Carter <kj.carter.design@gmail.com>
2023-08-21 23:21:56 -0400

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,10 @@
-//bring in mongoose so we can create a schema that represents the data for a User
+// Bring in mongoose so we can create a schema that represents the data for a User
 const mongoose = require("mongoose");
-//require bcrytp to help with password encryption
-const bcrypt = require("bcrypt")
+// Require bcrypt to help with password encryption
+const bcrypt = require("bcrypt");
 
-//Create our schema using mongoose that contains the fields and their data types for our Users
-//More info: https://mongoosejs.com/docs/schematypes.html
+// Create our schema using mongoose that contains the fields and their data types for our Users
+// More info: https://mongoosejs.com/docs/schematypes.html
 const userSchema = new mongoose.Schema({
     firstName: {
         type: String, 
@@ -28,33 +28,83 @@ const userSchema = new mongoose.Schema({
         type: String, 
         required: true,
         select: false
+    },
+    characters: [{
+        characterName: String,
+        level: {
+            type: Number,
+            default: 1
+        },
+        experience: {
+            type: Number,
+            default: 0
+        },
+        currentDeck: [{
+            cardId: mongoose.Schema.Types.ObjectId, // Reference to the card's unique ID
+            quantity: Number
+        }],
+        avatar: String // URL or path to avatar image or identifier for preset avatars
+    }],
+    ownedCards: [{
+        cardId: mongoose.Schema.Types.ObjectId, // Reference to the card's unique ID
+        quantity: Number
+    }],
+    decks: [{
+        deckName: String,
+        cards: [{
+            cardId: mongoose.Schema.Types.ObjectId, // Reference to the card's unique ID
+            quantity: Number
+        }]
+    }],
+    stats: {
+        wins: {
+            type: Number,
+            default: 0
+        },
+        losses: {
+            type: Number,
+            default: 0
+        }
+        // Other game-related metrics can be added here
+    },
+    achievements: [{
+        title: String,
+        description: String,
+        dateAchieved: Date
+    }],
+    settings: {
+        theme: {
+            type: String,
+            default: "default"
+        }
+        // Other user settings/preferences
     }
-})
+});
 
 userSchema.pre('save', function(next) {
     let user = this;
 
-    // only hash the password if it has been modified (or is new)
+    // Only hash the password if it has been modified (or is new)
     if (!user.isModified('password')) return next();
 
-    // generate a salt
+    // Generate a salt
     bcrypt.genSalt(10, function(err, salt) {
         if (err) return next(err);
 
-        // hash the password using our new salt
+        // Hash the password using our new salt
         bcrypt.hash(user.password, salt, function(err, hash) {
             if (err) return next(err);
-            // override the cleartext password with the hashed one
+            // Override the cleartext password with the hashed one
             user.password = hash;
             next();
         });
     });
 });
 
-//Generate the model our code with interact with from the above schema
-//Models allow us to interact with the data inside our MongoDB collections
-//More info: https://mongoosejs.com/docs/models.html
+// Generate the model our code with interact with from the above schema
+// Models allow us to interact with the data inside our MongoDB collections
+// More info: https://mongoosejs.com/docs/models.html
 const User = mongoose.model('User', userSchema);
 
-//export our model
+// Export our model
 module.exports = User;
