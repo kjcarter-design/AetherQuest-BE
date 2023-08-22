4ca24fcffe9caf8fa036ba79d81a0b02d774b143
Kevin Carter <kj.carter.design@gmail.com>
2023-08-21 23:21:33 -0400

    add cors for development

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors')
 
 const auth = require("./auth");
 
@@ -23,7 +24,7 @@ const usersRouter = require('./routes/user.routes');
 const swaggerDocsRouter = require("./routes/swagger.routes");
 
 const app = express();
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
